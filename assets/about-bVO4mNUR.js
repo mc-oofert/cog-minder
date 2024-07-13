9e1c00e375d94756f5bc2c745cb5f2163eb68f2a
mc-oofert <mc-oofert@users.noreply.github.com>
2024-07-13 16:42:57 +0000

    deploy: 15b3afeb76ea91df7efe4895cb385de854c18fa8

--- assets/about-bVO4mNUR.js
+++
@@ -1,1 +0,0 @@
-import"./common-H5yCEHFv.js";import{c as o,j as t,A as r}from"./App-OC44B1MB.js";import"./items-QxHqxA7B.js";import"./chart-s5gNgz83.js";import"./bots-t-eGkHsJ.js";const e=document.getElementById("root"),p=o(e);p.render(t.jsx(r,{pageType:"About"}));
